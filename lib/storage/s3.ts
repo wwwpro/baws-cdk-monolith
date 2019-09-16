83483ddf27ab58e61b9789f3831fa1d77685d32e
Dylan <dylan@illuminedigital.com>
2019-09-16 13:55:14 -0700

    =Bucket UUID repair.

--- lib/storage/s3.ts
+++ lib/storage/s3.ts
@@ -13,7 +13,8 @@ export class BawsS3 extends Stack {
 
     for (let i = 0; i < props.config.length; i++) {
       const configItem = props.config[i];
-      let bucketName = (configItem.addUniquId === true)?`configItem.name-${uuid().toLowerCase()}`: configItem.name ;
+      const bucketUUID = uuid();
+      let bucketName = (configItem.addUniquId === true)?`configItem.name-${bucketUUID.toLowerCase()}`: configItem.name.toLowerCase() ;
 
       const bucket = new CfnBucket(this, `baws-bucket-${configItem.name}`, {
         bucketName
